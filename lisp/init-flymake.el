3e4653d7447a9144f358e80088c46a60d2caf2b7
Brian Ramos <brirams@users.noreply.github.com>
2026-05-05 16:23:43 -0700

    stop proselinting my org files bro

--- lisp/init-flymake.el
+++ lisp/init-flymake.el
@@ -15,7 +15,9 @@
 
   (add-hook 'flymake-mode-hook 'flymake-flycheck-auto)
   (add-hook 'prog-mode-hook 'flymake-mode)
-  (add-hook 'text-mode-hook 'flymake-mode))
+  (add-hook 'text-mode-hook (lambda ()
+                              (unless (derived-mode-p 'org-mode)
+                                (flymake-mode)))))
 
 (with-eval-after-load 'flymake
   ;; Provide some flycheck-like bindings in flymake mode to ease transition
--- lisp/init-org.el
+++ lisp/init-org.el
@@ -116,9 +116,7 @@ typical word processor."
 (global-set-key (kbd "C-c c") 'org-capture)
 
 (setq org-default-notes-file (expand-file-name "~/data/notes/gtd/inbox.org"))
-(setq org-agenda-files (directory-files-recursively
-                        (file-truename (expand-file-name "~/data/notes/gtd/"))
-                        "\\.org$"))
+(setq org-agenda-files (directory-files (expand-file-name "~/data/notes/gtd/") t "\\.org$"))
 
 (setq org-capture-templates
       `(("t" "todo" entry (file "")  ; "" => `org-default-notes-file'
